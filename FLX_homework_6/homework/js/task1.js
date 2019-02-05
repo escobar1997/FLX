b537198b18a37c80f9739184745594c37bf877bc
Bohdan Tataryn <46561420+escobar1997@users.noreply.github.com>
2019-02-05 19:03:29 +0200

    Add files via upload

--- FLX_homework_6/homework/js/task1.js
+++ FLX_homework_6/homework/js/task1.js
@@ -1,20 +1,21 @@
-var a = prompt('Input a:');
-var b = prompt('Input b:');
-var c = prompt('Input c:');
-if (isNaN(a) || isNaN(b) || isNaN(c) || a === "" || b === "" || c === "") {
+var a = prompt('Input a:', 1);
+var b = prompt('Input b:', 1);
+var c = prompt('Input c:', -1);
+if (isNaN(a) || isNaN(b) || isNaN(c) || a === "" || b === "" || c === "" ||
+    a === "0" || a === null || b === null || c === null) {
   alert("Invalid input data");
 } else {
   var dysk = Math.pow(b, 2) - 4 * a * c;
   if (dysk > 0) {
-    var x1 = (-b - Math.sqrt(dysk)) / 2 * a;
-    var x2 = (-b + Math.sqrt(dysk)) / 2 * a;
+    var x1 = (-b - Math.sqrt(dysk)) / (2 * a);
+    var x2 = (-b + Math.sqrt(dysk)) / (2 * a);
     if (x1 === x2) {
       alert("x = " + x1);
     } else {
       alert("x1 = " + x1 + " and x2 = " + x2);
     }
   } else if (dysk === 0) {
-    alert("x = " + (-b / 2 * a));
+    alert("x = " + (-b / (2 * a)));
   } else if (dysk < 0){
     alert("no solution");
   }
