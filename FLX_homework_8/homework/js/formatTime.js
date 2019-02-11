51a08a6dd98607a62e48f203f17da1428455cac8
Bohdan Tataryn <46561420+escobar1997@users.noreply.github.com>
2019-02-11 20:32:37 +0200

    Add files via upload

--- FLX_homework_8/homework/js/formatTime.js
+++ FLX_homework_8/homework/js/formatTime.js
@@ -2,7 +2,7 @@ function formatTime(minutes) {
   var days = Math.floor(minutes / 1440);
   var hours = Math.floor((minutes % 1440) / 60);
   var newMinutes = (minutes % 1440) % 60;
-  console.log(days + " day(s) " + hours + " hour(s) " + newMinutes + " minute(s)");
+  return (days + " day(s) " + hours + " hour(s) " + newMinutes + " minute(s)");
 }
 formatTime(120);
 formatTime(59);
